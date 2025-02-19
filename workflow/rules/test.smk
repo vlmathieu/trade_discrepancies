ac941a29058130c701867cea7ff560afa33dba8d
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-02-19 11:07:31 +0100

    Removing outdated rules.

--- workflow/rules/test.smk
+++
@@ -1,7 +0,0 @@
-rule test:
-    params:
-        years   = config['years']
-    log:
-        'logs/test.log'
-    script:
-        'sandbox/toy_script.py'
--- workflow/scripts/merge_uncomtrade.py
+++
@@ -1,9 +0,0 @@
-from snakemake.script import snakemake
-import polars as pl
-
-data_merged = pl.read_parquet(snakemake.input['data'])
-
-data_merged.write_parquet(
-    snakemake.output[0],
-    compression='gzip'
-)
