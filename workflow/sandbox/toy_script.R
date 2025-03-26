10e00f274a9dc27df86552bd1d2e6c72e64a0b8c
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-03-26 16:25:46 +0100

    New toy script to play on R.

---
+++ workflow/sandbox/toy_script.R
@@ -0,0 +1,25 @@
+r_packages <- c("ggplot2",
+                "ggrepel",
+                "ggpubr",
+                "hrbrthemes",
+                "scales",
+                "patchwork",
+                "poweRlaw",
+                "maps",
+                "geosphere",
+                "CoordinateCleaner",
+                "dplyr",
+                "reshape2",
+                "FAOSTAT",
+                "utils")
+
+for (pkg in r_packages){
+  if (!require(pkg)) {
+    install.packages(pkg)
+    library(pkg)
+  }
+}
+
+for (pkg in r_packages){
+  print(pkg)
+}
