42e262dfa6d81a4f6c7e7794e1eca5f8be490c81
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-05-20 16:53:01 +0200

    More game more fun.

--- workflow/sandbox/toy_script.R
+++ workflow/sandbox/toy_script.R
@@ -37,243 +37,118 @@ library("showtext")
 library("ggtext")
 
 data <- read.csv(
-  file = "/Users/valentinmathieu/Desktop/wd/trade_discrepancies/results/processed_data/network_analysis/output/network_composition.csv",
+  file = "/Users/valentinmathieu/Desktop/wd/trade_discrepancies/results/processed_data/network_analysis/output/market_concentration.csv", # nolint
   header = TRUE,
   sep = ";")
 head(data)
 data[data$period == 2022 & data$cmd == 12, ]
 select(filter(data, period == 2022, cmd == 12), traded_value_exp)
 
-for (cmd in unique(data$cmd)) {
-  print(cmd)
-}
+pal <- c("#3D85F7", "#C32E5A")
+order <- c("hhi_imp", "hhi_exp")
 
-pal <- c("#3D85F7", "#A49393", "#C32E5A")
-order <- c("nb_pure_imp", "nb_mixed", "nb_pure_exp")
-
-plot_compo <- data %>%
+plot_mkt <- data %>%
   filter(cmd == 12) %>%
-  select(c("period", "nb_pure_exp", "nb_pure_imp", "nb_mixed")) %>%
+  select(c("period", "hhi_imp", "hhi_exp")) %>%
   melt(id.vars = "period",
        variable.name = "trader_type",
-       value.name = "nb_country") %>%
+       value.name = "hhi") %>%
   arrange(period) %>%
   mutate(trader_type = factor(trader_type, levels = order)) %>%
   ggplot(aes(x = period,
-             y = nb_country,
-             fill = trader_type,
+             y = hhi,
+             color = trader_type,
              label = trader_type)) +
-  # geom_stream(type = "ridge", bw = 1, alpha = 0.8) +
-  geom_area(alpha = 0.75) +
+  geom_line() +
 
   # End of chart labels
-  annotate("text", x = 2022.4,
-           y = data[data$period == 2022 &
-                      data$cmd == 12, ]$tot_nb_nodes -
-             data[data$period == 2022 &
-                    data$cmd == 12, ]$nb_pure_imp / 2,
-           label = paste(as.character(data[data$period == 2022 &
-                                             data$cmd == 12, ]$nb_pure_imp),
-                         "pure importers",
-                         sep = " "),
+  annotate("text", 
+           x = max(data$period) + 0.4,
+           y = data[data$period == max(data$period) &
+                      data$cmd == prod, ]$hhi_imp,
+           label = "Imports",
            hjust = 0,
            size = 3,
            lineheight = .8,
            fontface = "bold",
            color = pal[1]) +
 
-  annotate("text", x = 2022.4,
-           y = (data[data$period == 2022 &
-                       data$cmd == 12, ]$nb_mixed +
-                  data[data$period == 2022 &
-                         data$cmd == 12, ]$nb_pure_exp) / 2,
-           label = paste(as.character(data[data$period == 2022 &
-                                             data$cmd == 12, ]$nb_mixed),
-                         "mixed countries",
-                         sep = " "),
+  annotate("text",
+           x = max(data$period) + 0.4,
+           y = data[data$period == max(data$period) &
+                      data$cmd == prod, ]$hhi_exp,
+           label = "Exports",
            hjust = 0,
            size = 3,
            lineheight = .8,
            fontface = "bold",
            color = pal[2]) +
 
-  annotate("text", x = 2022.4,
-           data[data$period == 2022 &
-                  data$cmd == 12, ]$nb_pure_exp / 2,
-           label = paste(as.character(data[data$period == 2022 &
-                                             data$cmd == 12, ]$nb_pure_exp),
-                         "pure exporters",
-                         sep = " "),
+  # Segments
+  geom_segment(aes(x = min(data$period),
+                   y = 0.25,
+                   xend = max(data$period),
+                   yend = 0.25),
+               color = "black",
+               linetype = "dotted") +
+  annotate("text",
+           x = max(data$period) + 0.4,
+           y = 0.25,
+           label = "HHI = 0.25",
            hjust = 0,
            size = 3,
            lineheight = .8,
            fontface = "bold",
-           color = pal[3]) +
+           color = "black") +
 
-  # Segments
-  geom_segment(aes(x = 1996,
-                   y = 0,
-                   xend = 1996,
-                   yend = data[data$period == 1996 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 1996,
-                 y = data[data$period == 1996 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 1996,
-           y = data[data$period == 1996 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 1996 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
+  geom_segment(aes(x = min(data$period),
+                   y = 0.15,
+                   xend = max(data$period),
+                   yend = 0.15),
+               color = "black",
+               linetype = "dotted") +
+  annotate("text",
+           x = max(data$period) + 0.4,
+           y = 0.15,
+           label = "HHI = 0.15",
+           hjust = 0,
            size = 3,
            lineheight = .8,
            fontface = "bold",
            color = "black") +
 
-  geom_segment(aes(x = 2000,
-                   y = 0,
-                   xend = 2000,
-                   yend = data[data$period == 2000 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 2000,
-                 y = data[data$period == 2000 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 2000,
-           y = data[data$period == 2000 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 2000 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
+  geom_segment(aes(x = min(data$period),
+                   y = 0.01,
+                   xend = max(data$period),
+                   yend = 0.01),
+               color = "black",
+               linetype = "dotted") +
+  annotate("text",
+           x = max(data$period) + 0.4,
+           y = 0.01,
+           label = "HHI = 0.01",
+           hjust = 0,
            size = 3,
            lineheight = .8,
            fontface = "bold",
            color = "black") +
 
-  geom_segment(aes(x = 2005,
-                   y = 0,
-                   xend = 2005,
-                   yend = data[data$period == 2005 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 2005,
-                 y = data[data$period == 2005 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 2005,
-           y = data[data$period == 2005 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 2005 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
-           size = 3,
-           lineheight = .8,
-           fontface = "bold",
-           color = "black") +
-
-  geom_segment(aes(x = 2010,
-                   y = 0,
-                   xend = 2010,
-                   yend = data[data$period == 2010 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 2010,
-                 y = data[data$period == 2010 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 2010,
-           y = data[data$period == 2010 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 2010 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
-           size = 3,
-           lineheight = .8,
-           fontface = "bold",
-           color = "black") +
-
-  geom_segment(aes(x = 2015,
-                   y = 0,
-                   xend = 2015,
-                   yend = data[data$period == 2015 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 2015,
-                 y = data[data$period == 2015 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 2015,
-           y = data[data$period == 2015 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 2015 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
-           size = 3,
-           lineheight = .8,
-           fontface = "bold",
-           color = "black") +
-
-  geom_segment(aes(x = 2020,
-                   y = 0,
-                   xend = 2020,
-                   yend = data[data$period == 2020 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 2020,
-                 y = data[data$period == 2020 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 2020,
-           y = data[data$period == 2020 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 2020 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
-           size = 3,
-           lineheight = .8,
-           fontface = "bold",
-           color = "black") +
-
-  geom_segment(aes(x = 2022,
-                   y = 0,
-                   xend = 2022,
-                   yend = data[data$period == 2022 &
-                                 data$cmd == 12, ]$tot_nb_nodes),
-               color = "black") +
-  geom_point(aes(x = 2022,
-                 y = data[data$period == 2022 &
-                            data$cmd == 12, ]$tot_nb_nodes),
-             color = "black") +
-  annotate("text", x = 2022,
-           y = data[data$period == 2022 &
-                      data$cmd == 12, ]$tot_nb_nodes + 5,
-           label = as.character(data[data$period == 2022 &
-                                       data$cmd == 12, ]$tot_nb_nodes),
-           hjust = 0.5,
-           size = 3,
-           lineheight = .8,
-           fontface = "bold",
-           color = "black") +
-
-  scale_fill_manual(values = pal) +
+  scale_color_manual(values = pal) +
   scale_x_continuous(
     breaks = c(1996, 2000, 2005, 2010, 2015, 2020, 2022),
     labels = c("1996", "2000", "2005", "2010", "2015", "2020", "2022")
   ) +
-  scale_y_continuous(expand = c(0, 0)) +
+  scale_y_continuous(limits = c(0, 0.5), expand = c(0, 0)) +
   labs(x = "Year",
-       y = "Number of trading countries") +
+       y = "Herfindahl-Hirschman Index") +
   coord_cartesian(clip = "off") +
   theme_ipsum(axis_title_size = 11) +
   theme(legend.position = "none",
         plot.margin = margin(40, 80, 20, 20))
 
 ggsave(
-  "composition.png",
-  plot = plot_compo,
+  "mkt_concentration.png",
+  plot = plot_mkt,
   device = "png",
   path = "/Users/valentinmathieu/Desktop/",
   scale = 1,
@@ -284,3 +159,4 @@ ggsave(
   limitsize = TRUE,
   bg = "white"
 )
+
