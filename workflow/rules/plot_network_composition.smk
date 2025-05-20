76e136852a1369e0537dc52c1c7aece48e351fd4
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-05-20 11:57:33 +0200

    Update rule and script to save plot under different extension / format and add comments to code.

--- workflow/rules/plot_network_composition.smk
+++ workflow/rules/plot_network_composition.smk
@@ -2,10 +2,12 @@ rule plot_network_composition:
     input:
         'results/processed_data/network_analysis/output/network_composition.csv'
     output:
-        expand('results/processed_data/network_analysis/plot/{fao_div}/network_composition.png',
-               fao_div = config['fao_divisions'])
+        expand('results/processed_data/network_analysis/plot/{fao_div}/network_composition.{ext}',
+               fao_div = config['fao_divisions'],
+               ext = ['png', 'svg'])
     params:
-        fao_divisions   = config['fao_divisions']
+        fao_divisions   = config['fao_divisions'],
+        ext             = ['png', 'svg']
     threads: 1
     conda:
         '../envs/r_plots.yaml'
--- workflow/scripts/plot_network_composition.R
+++ workflow/scripts/plot_network_composition.R
@@ -3,35 +3,45 @@ library("hrbrthemes")
 library("dplyr")
 library("reshape2")
 
+# Load data
 data <- read.csv(file = snakemake@input[[1]],
                  header = TRUE,
                  sep = ";")
 
+# Define color palette
 pal <- c("#3D85F7", "#A49393", "#C32E5A")
-order <- c("nb_pure_imp", "nb_mixed", "nb_pure_exp")
 
-# for (prod in unique(data$cmd)) {
+# Define order or areas for stacked graph
+order <- c("nb_pure_imp", "nb_mixed", "nb_pure_exp")
 
 for (fao_division in snakemake@params$fao_divisions) {
 
+  # Convert fao_division to numeric to filter data
   prod <- as.numeric(fao_division)
 
+  # Data processing for plotting
   plot_compo <- data %>%
+    # Filter data for the given commodity
     filter(cmd == prod) %>%
+    # Select relevant columns
     select(c("period", "nb_pure_exp", "nb_pure_imp", "nb_mixed")) %>%
+    # Convert wide to long dataframe
     melt(id.vars = "period",
          variable.name = "trader_type",
          value.name = "nb_country") %>%
     arrange(period) %>%
+    # Convert trader_type to factor and order its values
     mutate(trader_type = factor(trader_type, levels = order)) %>%
+
+    # Create ggplot area plot
     ggplot(aes(x = period,
                y = nb_country,
                fill = trader_type,
                label = trader_type)) +
     geom_area(alpha = 0.75) +
 
-    # End of chart labels
-    annotate("text", x = 2022.4,
+    # End of chart labels = final values for each area
+    annotate("text", x = max(data$period) + 0.4,
              y = data[data$period == 2022 & data$cmd == prod, ]$tot_nb_nodes -
                data[data$period == 2022 & data$cmd == prod, ]$nb_pure_imp / 2,
              label = paste(as.character(select(filter(data,
@@ -46,12 +56,13 @@ for (fao_division in snakemake@params$fao_divisions) {
              fontface = "bold",
              color = pal[1]) +
 
-    annotate("text", x = 2022.4,
-             y = (data[data$period == 2022 & data$cmd == prod, ]$nb_mixed +
-                 data[data$period == 2022 & data$cmd == prod, ]$nb_pure_exp
-             ) / 2,
+    annotate("text", x = max(data$period) + 0.4,
+             y = (data[data$period == max(data$period) &
+                         data$cmd == prod, ]$nb_mixed +
+                    data[data$period == max(data$period) &
+                           data$cmd == prod, ]$nb_pure_exp) / 2,
              label = paste(as.character(select(filter(data,
-                                                      period == 2022,
+                                                      period == max(data$period), # nolint
                                                       cmd == prod),
                                                nb_mixed)),
                            "mixed countries",
@@ -62,10 +73,11 @@ for (fao_division in snakemake@params$fao_divisions) {
              fontface = "bold",
              color = pal[2]) +
 
-    annotate("text", x = 2022.4,
-             y = data[data$period == 2022 & data$cmd == prod, ]$nb_pure_exp / 2,
+    annotate("text", x = max(data$period) + 0.4,
+             y = data[data$period == max(data$period) &
+                        data$cmd == prod, ]$nb_pure_exp / 2,
              label = paste(as.character(select(filter(data,
-                                                      period == 2022,
+                                                      period == max(data$period), # nolint
                                                       cmd == prod),
                                                nb_pure_exp)),
                            "pure exporters",
@@ -76,21 +88,21 @@ for (fao_division in snakemake@params$fao_divisions) {
              fontface = "bold",
              color = pal[3]) +
 
-    # Segments
-    geom_segment(aes(x = 1996,
+    # Segments to highlight particular years and corresponding y values
+    geom_segment(aes(x = min(data$period),
                      y = 0,
-                     xend = 1996,
-                     yend = data[data$period == 1996 &
+                     xend = min(data$period),
+                     yend = data[data$period == min(data$period) &
                                    data$cmd == prod, ]$tot_nb_nodes),
                  color = "black") +
-    geom_point(aes(x = 1996,
-                   y = data[data$period == 1996 &
+    geom_point(aes(x = min(data$period),
+                   y = data[data$period == min(data$period) &
                               data$cmd == prod, ]$tot_nb_nodes),
                color = "black") +
-    annotate("text", x = 1996,
-             y = data[data$period == 1996 &
+    annotate("text", x = min(data$period),
+             y = data[data$period == min(data$period) &
                         data$cmd == prod, ]$tot_nb_nodes + 5,
-             label = as.character(data[data$period == 1996 &
+             label = as.character(data[data$period == min(data$period) &
                                          data$cmd == prod, ]$tot_nb_nodes),
              hjust = 0.5,
              size = 3,
@@ -203,20 +215,20 @@ for (fao_division in snakemake@params$fao_divisions) {
              fontface = "bold",
              color = "black") +
 
-    geom_segment(aes(x = 2022,
+    geom_segment(aes(x = max(data$period),
                      y = 0,
-                     xend = 2022,
-                     yend = data[data$period == 2022 &
+                     xend = max(data$period),
+                     yend = data[data$period == max(data$period) &
                                    data$cmd == prod, ]$tot_nb_nodes),
                  color = "black") +
-    geom_point(aes(x = 2022,
-                   y = data[data$period == 2022 &
+    geom_point(aes(x = max(data$period),
+                   y = data[data$period == max(data$period) &
                               data$cmd == prod, ]$tot_nb_nodes),
                color = "black") +
-    annotate("text", x = 2022,
-             y = data[data$period == 2022 &
+    annotate("text", x = max(data$period),
+             y = data[data$period == max(data$period) &
                         data$cmd == prod, ]$tot_nb_nodes + 5,
-             label = as.character(data[data$period == 2022 &
+             label = as.character(data[data$period == max(data$period) &
                                          data$cmd == prod, ]$tot_nb_nodes),
              hjust = 0.5,
              size = 3,
@@ -226,7 +238,7 @@ for (fao_division in snakemake@params$fao_divisions) {
 
     scale_fill_manual(values = pal) +
     scale_x_continuous(
-      breaks = c(1996, 2000, 2005, 2010, 2015, 2020, 2022),
+      breaks = c(min(data$period), 2000, 2005, 2010, 2015, 2020, max(data$period)), # nolint
       labels = c("1996", "2000", "2005", "2010", "2015", "2020", "2022")
     ) +
     scale_y_continuous(expand = c(0, 0)) +
@@ -237,21 +249,28 @@ for (fao_division in snakemake@params$fao_divisions) {
     theme(legend.position = "none",
           plot.margin = margin(40, 80, 20, 20))
 
-  ggsave(
-    basename(snakemake@output[[1]]),
-    plot = plot_compo,
-    device = "png",
-    path = paste(dirname(dirname(snakemake@output[[1]])),
-                 fao_division,
-                 sep = "/"),
-    create.dir = TRUE,
-    scale = 1,
-    width = 2480 * 1.1,
-    height = 1240 * 1.5,
-    units = c("px"),
-    dpi = 300,
-    limitsize = TRUE,
-    bg = "white"
-  )
+  # Save plot to all file extensions
+  for (ext in snakemake@params$ext) {
 
+    ggsave(
+      paste(sub(pattern = "(.*)\\..*$",
+                replacement = "\\1",
+                basename(snakemake@output[[1]])),
+            ext,
+            sep = "."),
+      plot = plot_compo,
+      device = ext,
+      path = paste(dirname(dirname(snakemake@output[[1]])),
+                   fao_division,
+                   sep = "/"),
+      create.dir = TRUE,
+      scale = 1,
+      width = 2480 * 1.1,
+      height = 1240 * 1.5,
+      units = c("px"),
+      dpi = 300,
+      limitsize = TRUE,
+      bg = "white"
+    )
+  }
 }
