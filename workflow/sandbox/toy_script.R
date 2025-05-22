3101c11c5b16367a62486251d1f506c2ed032ef0
vlmathieu <valentin.mathieu@agroparistech.fr>
2025-05-22 17:12:57 +0200

    More game more fun.

--- workflow/sandbox/toy_script.R
+++ workflow/sandbox/toy_script.R
@@ -38,80 +38,132 @@ library("showtext")
 library("ggtext")
 
 data <- read.csv(
-  file = "/Users/valentinmathieu/Desktop/wd/trade_discrepancies/results/processed_data/network_analysis/output/market_concentration.csv", # nolint
+  file = "/Users/valentinmathieu/Desktop/wd/trade_discrepancies/results/processed_data/network_analysis/output/network_connectivity.csv", # nolint
   header = TRUE,
   sep = ";")
 head(data)
-to_match <- paste0("primary_value", c("_imp", "_exp"))
-size_col <- colnames(data)[grep(paste(to_match, collapse = "|"), colnames(data))]
+summary(data)
 
 pal <- c("#3D85F7", "#C32E5A")
-columns <- c("country",
-             "nb_edge_imp",
-             "nb_edge_exp",
-             "primary_value_imp",
-             "primary_value_exp")
 
-plot_profile <- data %>%
-  filter(cmd == 12, period == 2001) %>%
-  select(all_of(columns)) %>%
-  mutate(across(all_of(size_col), ~ .x / 1000000)) %>%
-  arrange(desc(size_col[grep("_imp", size_col)])) %>%
-  setNames(gsub("primary_value", "size", names(.))) %>%
-  mutate(country = factor(country)) %>%
+lst <- gsub("_exp", "", names(data)[grep("exp", names(data))])
+metrics <- lst[lst != "nb_edge"]
 
-  ggplot(aes(x = nb_edge_exp,
-             y = nb_edge_imp,
-             label = country)) +
+myplots <- list()
 
-  geom_segment(aes(x = 0,
-                   y = 0,
-                   xend = ceiling(max(data$nb_edge_exp) / 10) * 10,
-                   yend = ceiling(max(data$nb_edge_imp) / 10) * 10),
-               color = "grey",
-               linetype = "dotted") +
+for (metric in metrics) {
 
-  geom_point(aes(size = size_exp), alpha = 0.6, color = pal[2]) +
-  geom_point(aes(size = size_imp), alpha = 0.6, color = pal[1]) +
+  # Select columns for plot
+  columns <- c("period", paste0(metric, c("_imp", "_exp")))
 
-  geom_text_repel(size = 2,
-                  min.segment.length = 1.2,
-                  position = position_nudge_repel(x = 3, y = 3),
-                  fontface = "bold") +
-  geom_label(label = 2022, x = 114, y = 4, size = 5) +
+  # Define order for corresponding colors
+  order <- paste0(metric, c("_imp", "_exp"))
 
-  scale_size(range = c(1, 20),
-             breaks = c(1000, 2000, 5000, 10000),
-             limits = c(0, 20000),
-             name = "Traded value (million US$)") +
-  scale_x_continuous(name = "Number of export partners",
-                     limits = c(0, ceiling(max(data$nb_edge_exp) / 10) * 10),
-                     breaks = seq(0,
-                                  ceiling(max(data$nb_edge_exp) / 10) * 10,
-                                  by = 20),
-                     expand = c(0, 0)) +
-  scale_y_continuous(name = "Number of import partners",
-                     limits = c(0, 120),
-                     breaks = seq(20,
-                                  ceiling(max(data$nb_edge_exp) / 10) * 10,
-                                  by = 20),
-                     expand = c(0, 0)) +
-  theme_ipsum(axis_title_size = 11)
+  # Define name for y axis
+  if ("mean" %in% metric){
+    name <- "Mean"
+  } else if ("var" %in% metric) {
+    name <- "Variance"
+  } else if ("skew" %in% metric) {
+    name <- "Skewness"
+  } else if ("kurt" %in% metric) {
+    name <- "Kurtosis"
+  }
+
+  # Produce basic plot
+  plot_connectivity <- data %>%
+    filter(cmd == 12) %>%
+    # Select relevant columns
+    select(all_of(columns)) %>%
+    # Convert wide to long dataframe
+    melt(id.vars = "period",
+         variable.name = "trade_flow",
+         value.name = "metric") %>%
+    arrange(period) %>%
+    # Convert trader_type to factor and order its values
+    mutate(trade_flow = factor(trade_flow, levels = order)) %>%
+
+    # Create ggplot line plot
+    ggplot(aes(x = period,
+               y = metric,
+               color = trade_flow,
+               label = trade_flow)) +
+    geom_point(alpha = 0.6) +
+    # geom_line(alpha = 0.6) +
+    geom_smooth(method = NULL,
+                se = FALSE,
+                linewidth = 1)
+
+  # Collect final y values of geom_smooth to plot legend label
+  y_imp <- filter(ggplot_build(plot_connectivity)$data[[2]],
+                  x == max(data$period) & grepl("_imp", label))$y
+  y_exp <- filter(ggplot_build(plot_connectivity)$data[[2]],
+                  x == max(data$period) & grepl("_exp", label))$y
+
+  # Collect y_min and y_max for setting axis limits
+  y_min <- min(min(ggplot_build(plot_connectivity)$data[[2]]$y),
+               min(select(filter(data, cmd == 12), contains(metric))))
+  y_max <- max(max(ggplot_build(plot_connectivity)$data[[2]]$y),
+               round(max(select(filter(data, cmd == 12), contains(metric)))))
+
+  # Complete plot with annotation and theme
+  plot_connectivity <- plot_connectivity +
+    # End of chart labels
+    annotate("text",
+             x = max(data$period) + 0.4,
+             y = y_imp,
+             label = "Imports",
+             hjust = 0,
+             size = 3,
+             lineheight = .8,
+             fontface = "bold",
+             color = pal[1]) +
+
+    annotate("text",
+             x = max(data$period) + 0.4,
+             y = y_exp,
+             label = "Exports",
+             hjust = 0,
+             size = 3,
+             lineheight = .8,
+             fontface = "bold",
+             color = pal[2]) +
+
+    # Set up scale colors, breaks, and limits + themes
+    scale_color_manual(values = pal) +
+    scale_x_continuous(
+      breaks = c(1996, 2000, 2005, 2010, 2015, 2020, 2022),
+      labels = c("1996", "2000", "2005", "2010", "2015", "2020", "2022")
+    ) +
+    scale_y_continuous(limits = c(trunc(y_min - 1),
+                                  round(y_max + 1)),
+                       expand = c(0, 0)) +
+    labs(x = "Year",
+         y = paste0(name,
+                    " of the nb. of trading partners")) +
+    coord_cartesian(clip = "off") +
+    theme_ipsum(axis_title_size = 11) +
+    theme(legend.position = "none",
+          plot.margin = margin(20, 20, 20, 20))
+
+  # Store plot in list
+  myplots[[match(metric, metrics)]] <- plot_connectivity
+}
+
+# Produce composite plot using patchwork
+final_plot <- myplots[[1]] + myplots[[2]] + myplots[[3]] + myplots[[4]] +
+  plot_layout(ncol = 2)
 
 ggsave(
-  "contributor_profiles.png",
-  plot = plot_profile,
+  "connectivity.png",
+  plot = final_plot,
   device = "png",
   path = "/Users/valentinmathieu/Desktop/",
   scale = 1,
-  width = 2480 * 1.1,
-  height = 1240 * 1.5,
+  width = 2480 * 1.5,
+  height = 1240 * 2,
   units = c("px"),
   dpi = 300,
   limitsize = TRUE,
   bg = "white"
 )
-
-prod = 12
-round(max(data[data$cmd == prod, ]$hhi_imp, data[data$cmd == prod, ]$hhi_exp), 1) # nolint
-round(0.52 + 0.05, 1)
